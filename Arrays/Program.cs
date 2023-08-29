7278ae244565459d280570956dd29e881abb7e4f
sanya gupta <sanya.gupta@celebaltech.com>
2023-08-29 15:26:41 +0530

    first commit

---
+++ Arrays/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Schema;
+
+namespace Arrays
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int[] a = { 10, 20, 30 };
+               for(int i=0;i<a.Length;i++)
+               {
+                   Console.WriteLine(a[i]);
+               }  */
+
+
+
+
+
+          /*       string[] name = { "sanya", "gupta" };
+                      foreach(string i in name)
+                        {
+                            Console.WriteLine(i);
+                        }
+            */
+
+
+
+            /*  int[] b=new int[4];
+              Console.WriteLine("enter the values of array");
+              for(int i=0;i<b.Length;i++)
+              {   
+                  b[i] = int.Parse(Console.ReadLine());
+              }
+              Console.WriteLine("value of arrays:");
+              for (int i = 0; i < b.Length; i++)
+              {
+                  Console.WriteLine(b[i]);
+              }
+
+              //sort the array and also find the max and min element.
+              Array.Sort(b);
+              Console.WriteLine("sorted array");
+              for (int i=0;i<b.Length;i++)
+              {
+                  Console.WriteLine(b[i]);
+              }
+              Console.WriteLine("Maximum elemnet in array:"+b.Max());
+              Console.WriteLine("Minimum element in array:"+b.Min());  */
+
+
+
+
+           /*  int[,] n = { { 1, 4, 2 }, { 3, 6, 8 } };
+             Console.WriteLine(n[0, 2]); */
+            
+
+
+             int[,] n = { { 1, 4, 2 }, { 3, 6, 8 }};
+               for (int i = 0; i < 2; i++)
+               {
+                   for (int j = 0; j < 3; j++)
+                   {
+                       Console.WriteLine(n[i, j]);
+
+                   }
+               }  
+
+
+
+            /*  int n = 5;
+              for (int i = 1; i <= n; i++)
+              {
+                  for (int j = 1; j <= i; j++)
+                  {
+                      Console.Write("* ");
+                  }
+                  Console.WriteLine();
+              }*/
+
+
+
+            /*   int n = 5;
+               for (int i = 1; i <= n; i++)
+               {
+                   for (int j = 1; j <= n; j++)
+                   {
+                       Console.Write(i + " ");
+                   }
+                   Console.WriteLine();
+               }*/
+
+
+
+          /*     int n = 5;
+               for (int i = 1; i <= n; i++)
+               {
+                   for (int j = n - i; j > 0; j--)
+                   {
+                       Console.Write("  ");
+                   }
+
+                   for (int k = 1; k <= i; k++)
+                   {
+                       Console.Write("* ");
+                   }
+                   Console.WriteLine();
+               }
+              */
+
+
+
+            
+        }
+
+        }
+}
